fcba1701a3b06ef5d182739df386faf34626fa88
mdubrey <mdubrey@users.noreply.github.com>
2016-10-20 22:21:58 -0400

    adding a file for Monica
    created a function within the original code to be added to the branch

---
+++ HelloWorldMonica.java
@@ -0,0 +1,31 @@
+/*
+ *Source code midterm for CIS 2321 for Dr. Holbert
+ *Participants are Ryan Conway, Elijah Turner
+ *Kevin Westberry, Monica Dubrey
+ * the goal is to learn to use a source code version software
+ */
+package helloworld;
+
+/**
+ *
+ * @authors Elijah Turner, Ryan Conway, Kevin Westberry, Monica Dubrey
+ */
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("\nHello World!");
+        System.out.println("\nClass Collaboration Begins" );
+        monicaFunction();
+        // \n adds a new line
+    }
+    
+    public static void monicaFunction()
+    {
+        System.out.println("\nMonica has added a function to the source code.");
+        System.out.println("\nMonica also added new lines and comments.\n");
+    }
+     
+        
+}
